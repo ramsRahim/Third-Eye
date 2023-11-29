75e753dd3e026d77506f538e6e33eb12611c7426
ramsRahim <rahimhossain23@gmail.com>
2023-11-29 18:12:07 -0500

    changes to the reciever script

--- lora_reciever.py
+++ lora_reciever.py
@@ -28,12 +28,34 @@ old_settings = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin.fileno())
 
 node = sx126x.sx126x(serial_num = "/dev/ttyS0",freq=868,addr=0,power=22,rssi=True,air_speed=2400,relay=False)
+def receive_and_decode():
+    # Receive data from LoRa
+    data = node.receive()
+    
+    if data:
+        # Decode the data structure
+        recv_node_high = data[0]
+        recv_node_low = data[1]
+        offset_frequence = data[2]
+        send_node_high = data[3]
+        send_node_low = data[4]
+        offset_freq_sender = data[5]
 
+        # Reconstruct the addresses and the message
+        recv_node_addr = (recv_node_high << 8) | recv_node_low
+        send_node_addr = (send_node_high << 8) | send_node_low
+        message = data[6:].decode('utf-8', errors='ignore')  # Decoding the message as UTF-8
 
+        print(f"Received message: {message}")
+        print(f"From node address: {send_node_addr}")
+        print(f"To node address: {recv_node_addr}")
+
+# Main loop to continuously receive data
+    
 try:
     print("The receiver is listening ...")
     while True:        
-        node.receive()
+        receive_and_decode()
         
 except:
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
