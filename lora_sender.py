323c24b13e606625b2a9edc4a2935cfe3fbf96ae
ramsRahim <rahimhossain23@gmail.com>
2023-12-04 09:20:52 -0500

    Final Commit

--- lora_sender.py
+++ lora_sender.py
@@ -17,13 +17,10 @@
 
 import sys
 import sx126x
-import threading
 import time
-import select
 import termios
 import tty
-from threading import Timer
-import subprocess
+
 
 old_settings = termios.tcgetattr(sys.stdin)
 tty.setcbreak(sys.stdin.fileno())
@@ -39,12 +36,6 @@ tty.setcbreak(sys.stdin.fileno())
 
 
 #    The following is to obtain the temprature of the RPi CPU 
-def get_cpu_temp():
-    tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
-    cpu_temp = tempFile.read()
-    tempFile.close()
-    return float(cpu_temp)/1000
-
 def read_latest_label(file_path):
     # Read the last line from the file
     with open(file_path, 'r') as file:
@@ -75,56 +66,6 @@ def send_label(label):
 
 # File path to the labels file
 labels_file_path = './pred_labels.txt'
-
-def send_deal():
-    get_rec = ""
-    print("")
-    print("input a string such as \033[1;32m0,868,Hello World\033[0m,it will send `Hello World` to lora node device of address 0 with 868M ")
-    print("please input and press Enter key:",end='',flush=True)
-
-    while True:
-        rec = sys.stdin.read(1)
-        if rec != None:
-            if rec == '\x0a': break
-            get_rec += rec
-            sys.stdout.write(rec)
-            sys.stdout.flush()
-
-    get_t = get_rec.split(",")
-
-    offset_frequence = int(get_t[1])-(850 if int(get_t[1])>850 else 410)
-    #
-    # the sending message format
-    #
-    #         receiving node              receiving node                   receiving node           own high 8bit           own low 8bit                 own 
-    #         high 8bit address           low 8bit address                    frequency                address                 address                  frequency             message payload
-    data = bytes([int(get_t[0])>>8]) + bytes([int(get_t[0])&0xff]) + bytes([offset_frequence]) + bytes([node.addr>>8]) + bytes([node.addr&0xff]) + bytes([node.offset_freq]) + get_t[2].encode()
-
-    node.send(data)
-    print('\x1b[2A',end='\r')
-    print(" "*200)
-    print(" "*200)
-    print(" "*200)
-    print('\x1b[3A',end='\r')
-
-def send_cpu_continue(continue_or_not = True):
-    if continue_or_not:
-        global timer_task
-        global seconds
-        #
-        # boarcast the cpu temperature at 868.125MHz
-        #
-        data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes([12]) + "CPU Temperature:".encode()+str(get_cpu_temp()).encode()+" C".encode()
-        node.send(data)
-        time.sleep(0.2)
-        timer_task = Timer(seconds,send_cpu_continue)
-        timer_task.start()
-    else:
-        data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes([12]) + "CPU Temperature:".encode()+str(get_cpu_temp()).encode()+" C".encode()
-        node.send(data)
-        time.sleep(0.2)
-        timer_task.cancel()
-        pass
 
 try:
     
